cfa3c029eb4a44a098ab96f89158289139ba4824
queen <Queenmoncler@gmail.com>
2023-09-09 15:42:35 +0300

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,9 +1,22 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
-    public int add(String name, long number){
-        return 0;
+    Map<String, Long> phoneBook = new TreeMap<>();
+
+    public int add(String name, long number) {
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name, number);
+        }
+        return phoneBook.size();
     }
 
-    public String findByNumber (long number){
-        return null;
+    public String findByNumber(long number) {
+        return phoneBook.entrySet()
+                .stream()
+                .filter((x) -> x.getValue().equals(number))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
     }
 }
